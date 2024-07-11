857104d503a48856d0e076957c5ef6fd2f261952
ShinyaOkazawa <fess3idm@gmail.com>
2024-07-12 01:36:48 +0900

    format

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'histoire'
-import { HstVue } from '@histoire/plugin-vue'
-import { HstNuxt } from '@histoire/plugin-nuxt'
+import { HstNuxt } from "@histoire/plugin-nuxt";
+import { HstVue } from "@histoire/plugin-vue";
+import { defineConfig } from "histoire";
 
 export default defineConfig({
-  plugins: [
-    HstVue(),
-    HstNuxt(),
-  ],
-})
+	plugins: [HstVue(), HstNuxt()],
+});
