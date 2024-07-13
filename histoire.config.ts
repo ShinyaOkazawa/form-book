16d9ae9595ffb94f4e8c3c6586fe81ea01709b7c
ShinyaOkazawa <fess3idm@gmail.com>
2024-07-14 03:17:50 +0900

    feat: histoire に global css を読み込み

--- histoire.config.ts
+++ histoire.config.ts
@@ -3,5 +3,6 @@ import { HstVue } from "@histoire/plugin-vue";
 import { defineConfig } from "histoire";
 
 export default defineConfig({
+  setupFile: "./histoire.setup.ts",
   plugins: [HstVue(), HstNuxt()],
 });
