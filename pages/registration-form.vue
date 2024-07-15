4bb36827a0dea952a5488df774e65300f3221e0e
ShinyaOkazawa <fess3idm@gmail.com>
2024-07-15 17:21:12 +0900

    feat: 登録フォーム追加

---
+++ pages/registration-form.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>登録フォーム</h1>
+  <div>
+    <form action="">
+      <TextField id="name" label="お名前" class="text-field" />
+      <TextField type="email" id="email" label="メールアドレス" hint="数字と大文字をそれぞれ1文字以上含めて、8文字以上で入力してください。"
+        class="text-field" />
+      <TextField type="password" id="password" label="パスワード" class="text-field" />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.text-field {
+  margin-top: var(--space-32);
+}
+</style>
