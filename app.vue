361d4f2f01105bfe29ab00ba89514e936bde0b6f
ShinyaOkazawa <fess3idm@gmail.com>
2024-07-15 16:50:47 +0900

    feat: 基本のCSSを追加

--- app.vue
+++ app.vue
@@ -1,4 +1,13 @@
 <template>
-  <div>
+  <div class="wrapper">
+    <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  width: 960px;
+  margin: auto;
+  padding: var(--space-32);
+}
+</style>
