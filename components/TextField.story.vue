2ccc63df411824e29a0b9b87b5fb4067d16f5813
ShinyaOkazawa <fess3idm@gmail.com>
2024-07-15 16:58:06 +0900

    feat: テキストフィールドコンポーネントを追加

---
+++ components/TextField.story.vue
@@ -0,0 +1,12 @@
+<template>
+  <Story>
+    <Variant title="default">
+      <TextField id="id" label="ラベル" />
+    </Variant>
+    <Variant title="hint">
+      <TextField id="id" label="ラベル" hint="テキストを入力してください" />
+    </Variant>
+  </Story>
+</template>
+
+<docs lang="md"></docs>
---
+++ components/TextField.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  hint: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: 'text'
+  }
+});
+</script>
+
+<template>
+  <div>
+    <label :for="id">
+      <span class="label">{{ label }}</span>
+      <span v-if="hint" class="hint">{{ hint }}</span>
+    </label>
+    <VInput :id="id" :name="id" :type="type" />
+  </div>
+</template>
+
+<style scoped>
+label {
+  font-size: var(--text-size-normal);
+}
+
+.label {
+  display: block;
+  margin-bottom: var(--space-4);
+  font-weight: bold;
+}
+
+.hint {
+  display: block;
+  margin-bottom: var(--space-4);
+}
+</style>
