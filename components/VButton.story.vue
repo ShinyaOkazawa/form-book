859496e5d69b4d95b7721ea85df594426708d0f6
ShinyaOkazawa <fess3idm@gmail.com>
2024-07-17 00:29:38 +0900

    feat: button コンポーネント追加

---
+++ components/VButton.story.vue
@@ -0,0 +1,9 @@
+<template>
+  <Story>
+    <Variant title="default">
+      <VButton>button</VButton>
+    </Variant>
+  </Story>
+</template>
+
+<style scoped></style>
---
+++ components/VButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  min-width: 120px;
+  padding: var(--space-8);
+}
+</style>
